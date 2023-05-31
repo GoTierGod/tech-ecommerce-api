864651e257e4551391e5f13a2310964372481cc0
gotiergod <ivan.gunlove_10_@hotmail.com>
2023-05-31 16:54:15 -0400

    test returning a username from the API

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+
 
 # Create your views here.
+@api_view()
+def welcome(request):
+    return Response(User.objects.get(pk=1).username)
