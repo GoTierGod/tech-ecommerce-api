d7ec618319ea170297bd0238a3d74c45fe69121d
gotiergod <ivan.gunlove_10_@hotmail.com>
2023-06-03 17:28:45 -0400

    removed rest framework filters import

--- api/views.py
+++ api/views.py
@@ -3,7 +3,6 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework import generics
-from rest_framework import filters
 from rest_framework import viewsets
 from . import serializers
 from . import models
