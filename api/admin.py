34660536fb91215b28a2957d4fe961ce607fdb4c
gotiergod <ivan.gunlove_10_@hotmail.com>
2023-06-06 22:36:58 -0400

    improved tables details in the admin site

--- api/admin.py
+++ api/admin.py
@@ -5,44 +5,74 @@ from . import models
 # Register your models here.
 @admin.register(models.Category)
 class Category(admin.ModelAdmin):
-    list_display = ("id", "title")
+    list_display = (
+        "id",
+        "title",
+    )
+    list_display_links = ("title",)
 
 
 @admin.register(models.Brand)
 class Brand(admin.ModelAdmin):
-    list_display = ("id", "name")
+    list_display = ("id", "name", "website_url")
+    list_display_links = ("name",)
 
 
 @admin.register(models.Customer)
 class Customer(admin.ModelAdmin):
-    list_display = ("id", "user")
+    list_display = ("id", "get_username", "get_email")
+    list_display_links = ("get_username",)
+
+    def get_username(self, obj):
+        return obj.user.username
+
+    def get_email(self, obj):
+        return obj.user.email
+
+    get_username.short_description = "Username"
+    get_email.short_description = "Email"
 
 
 @admin.register(models.DeliveryMan)
 class DeliveryMan(admin.ModelAdmin):
-    list_display = ("id", "user")
+    list_display = ("id", "get_username", "get_email")
+    list_display_links = ("get_username",)
+
+    def get_username(self, obj):
+        return obj.user.username
+
+    def get_email(self, obj):
+        return obj.user.email
+
+    get_username.short_description = "Username"
+    get_email.short_description = "Email"
 
 
 @admin.register(models.Product)
 class Product(admin.ModelAdmin):
-    list_display = ("id", "name", "price")
+    list_display = ("id", "name", "price", "brand", "category")
+    list_display_links = ("name",)
 
 
 @admin.register(models.ProductSpecification)
 class ProductSpecification(admin.ModelAdmin):
     list_display = ("id", "key", "value", "product")
+    list_display_links = ("key",)
 
 
 @admin.register(models.ProductImage)
 class ProductImage(admin.ModelAdmin):
     list_display = ("id", "product", "description")
+    list_display_links = ("product",)
 
 
 @admin.register(models.Review)
 class Review(admin.ModelAdmin):
     list_display = ("id", "product", "customer", "rating")
+    list_display_links = ("product",)
 
 
 @admin.register(models.Order)
 class Order(admin.ModelAdmin):
-    list_display = ("id", "product", "country", "city")
+    list_display = ("id", "product", "customer", "country", "city")
+    list_display_links = ("product",)
