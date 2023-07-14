a9598df6050a7463d068604534635fd3ea7d28f6
gotiergod <ivan.gunlove_10_@hotmail.com>
2023-07-14 11:42:17 -0400

    encrypted username in the jwt

--- project/views.py
+++ project/views.py
@@ -1,7 +1,27 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-@api_view()
-def home(request):
-    return Response({"message": "Bad request"}, status=400)
+
+@api_view(["GET"])
+def get_routers(request):
+    routes = ["/api/token/", "/api/token/refresh"]
+
+    return Response(routes)
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token["username"] = user.username
+
+        return token
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
