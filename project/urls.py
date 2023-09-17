c310d6463d092bcbb9717e849f8eecdbed1446cc
GoTierGod <ivan.gunlove_10_@hotmail.com>
2023-09-17 09:51:13 -0300

    only allow access to admin site on debug mode

--- project/urls.py
+++ project/urls.py
@@ -6,12 +6,17 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
 )
 from . import views
+from django.conf import settings
 
 urlpatterns = [
     path("", views.routes),
-    path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/token/blacklist/", TokenBlacklistView.as_view(), name="token_blacklist"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("admin/", admin.site.urls),
+    ]
